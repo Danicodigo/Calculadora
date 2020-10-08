1ee0932f35518a99823c3cfe9fe145007ba24148
XByDa <XByDa@DESKTOP-ASCHPDT>
2020-10-08 18:17:22 +0200

    sa

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,184 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.WindowConstants;
+import javax.swing.border.EmptyBorder;
+
+/**
+ * 
+ * Interfaz para nuestra calculadora sencilla
+ * 
+ * @author: $Author: franchux $
+ * @version: $Rev: 75 $
+ * @date: $Date: 2009-04-14 00:21:18 +0200 (mar 14 de abr de 2009) $ $Id:
+ *        VentanaCalculadora.java 75 2009-04-13 22:21:18Z franchux $
+ */
+public class Calculadora extends JFrame {
+
+	/**
+	 * generado
+	 */
+	private static final long serialVersionUID = 1583724102189855698L;
+
+	/** numero tecleado */
+	JTextField pantalla;
+
+	/** guarda el resultado de la operacion anterior o el número tecleado */
+	double resultado;
+
+	/** para guardar la operacion a realizar */
+	String operacion;
+
+	/** Los paneles donde colocaremos los botones */
+	JPanel panelNumeros, panelOperaciones,panel;
+
+	/** Indica si estamos iniciando o no una operación */
+	boolean nuevaOperacion = true;
+
+	/**
+	 * Constructor. Crea los botones y componentes de la calculadora
+	 */
+	public Calculadora() {
+		super();
+		setSize(250, 300);
+		setTitle("Calculadora Simple");
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		setResizable(false);
+
+		// Vamos a dibujar sobre el panel
+		panel = (JPanel) this.getContentPane();
+		panel.setLayout(new BorderLayout());
+
+		pantalla = new JTextField("0",20);
+                //Coloca el texto a la derecha
+		pantalla.setHorizontalAlignment(JTextField.RIGHT);
+		pantalla.setEditable(false);
+                //Posiciona el panel arriba
+		panel.add("North", pantalla);
+
+		panelNumeros = new JPanel();
+		panelNumeros.setLayout(new GridLayout(4, 3));
+                //Cambiar el tamaño de los bordes
+		panelNumeros.setBorder(new EmptyBorder(4, 4, 4, 4));
+
+		for (int i = 9; i >= 0; i--) {
+			JButton btn = new JButton();
+		btn.setText(i+"");
+		btn.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseReleased(MouseEvent evt) {
+				JButton btn = (JButton) evt.getSource();
+				numeroPulsado(btn.getText());
+			}
+		});
+
+		panelNumeros.add(btn);
+		}
+                JButton btn = new JButton();
+		btn.setText(".");
+		btn.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseReleased(MouseEvent evt) {
+				JButton btn = (JButton) evt.getSource();
+				numeroPulsado(btn.getText());
+			}
+		});
+
+		panelNumeros.add(btn);
+
+		panel.add("Center", panelNumeros);
+
+		panelOperaciones = new JPanel();
+		panelOperaciones.setLayout(new GridLayout(6, 1));
+		panelOperaciones.setBorder(new EmptyBorder(4, 4, 4, 4));
+
+		nuevaOperacion("+");
+		nuevaOperacion("-");
+		nuevaOperacion("*");
+		nuevaOperacion("/");
+		nuevaOperacion("=");
+		nuevaOperacion("CE");
+
+		panel.add("East", panelOperaciones);
+
+		validate();
+	}
+
+	private void nuevaOperacion(String operacion) {
+		JButton btn = new JButton(operacion);
+		btn.setForeground(Color.RED);
+
+		btn.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseReleased(MouseEvent evt) {
+				JButton btn = (JButton) evt.getSource();
+				operacionPulsado(btn.getText());
+			}
+		});
+
+		panelOperaciones.add(btn);
+	}
+
+	private void numeroPulsado(String digito) {
+		if (pantalla.getText().equals("0") || nuevaOperacion) {
+			pantalla.setText(digito);
+		} else {
+			pantalla.setText(pantalla.getText() + digito);
+		}
+		nuevaOperacion = false;
+	}
+
+	
+	private void operacionPulsado(String tecla) {
+		if (tecla.equals("=")) {
+			calcularResultado();
+		} else if (tecla.equals("CE")) {
+			resultado = 0;
+			pantalla.setText("");
+			nuevaOperacion = true;
+		} else {
+			operacion = tecla;
+			if ((resultado > 0) && !nuevaOperacion) {
+				calcularResultado();
+			} else {
+				resultado = new Double(pantalla.getText());
+			}
+		}
+
+		nuevaOperacion = true;
+	}
+
+	
+	private void calcularResultado() {
+		if (operacion.equals("+")) {
+			resultado += new Double(pantalla.getText());
+		} else if (operacion.equals("-")) {
+			resultado -= new Double(pantalla.getText());
+		} else if (operacion.equals("/")) {
+			resultado /= new Double(pantalla.getText());
+		} else if (operacion.equals("*")) {
+			resultado *= new Double(pantalla.getText());
+		}
+
+		pantalla.setText("" + resultado);
+		operacion = "";
+	}
+}
+
